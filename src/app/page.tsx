6954cdbe93a9de3064db39971114290252b96519
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-02-29 23:07:22 +0300

    style: delete margin in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>  
-    <h2 className="justify-center items-center m-auto">Welcome to Hackaton Inter-University of Madagascar</h2>
+    <h2 className="justify-center items-center">Welcome to Hackaton Inter-University of Madagascar</h2>
     <Link href="/signup"><Button>Click Here</Button></Link>
     </>
   )
