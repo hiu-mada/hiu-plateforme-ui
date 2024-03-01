9d18cdccfc90a145d1a025be41e0e0b172de2ecf
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-03-01 08:45:50 +0300

    fix; rename folder and now, i have visual

---
+++ src/app/(auth)/signup/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+
+export const metadata: Metadata ={
+    title : "HIU Madagascar",
+    description: "Sign up for HUI Platform",
+}
+
+const Layout = ({
+    children,
+}:{
+    children:React.ReactNode
+}) => {
+    return (
+        <>
+        <NextTopLoader color="#000" showSpinner={false}/>
+        {children}
+        </>
+    );
+};
+
+export default Layout;
