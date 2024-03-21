a2be643d91deb7fed6b18690321ce1a26ce7d90a
miharyjoel <miharyjoel@gmail.com>
2024-03-21 21:44:04 +0300

    feat: redising list participant

--- src/components/mock/ParticipantListMock.ts
+++ src/components/mock/ParticipantListMock.ts
@@ -1,152 +1,209 @@
 export const ChallengerTeam = [
-    {
+  {
+    id: 1,
+    name: "team1",
+    slogan: "Slogan1",
+    subject: {
+      name: "sujet 1",
+      description: "description",
+    },
+    User: [
+      {
         id: 1,
-        name: "ame1",
-        slogan: "Slogan1",
-        subject: {
-            name: "sujet 1",
-            description:  "description",
-        },
-        User: [
-            {
-                id: 1,
-                username: 'user1',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/images.png'
-            },
-            {
-                id: 2,
-                username: 'user1',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/images.png'
-            },
-            {
-                id: 3,
-                username: 'user1',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/images.png'
-            },
-            {
-                id: 4,
-                username: 'user1',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/images.png'
-            },
-            {
-                id: 5,
-                username: 'user1',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/images.png'
-            },
-            {
-                id: 6,
-                username: 'user1',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/image.png'
-            },
-        ],
-        University:
-        {
-            name: "niversity1",
-            url:'/HEI.png'
-        },
-        votes: 0,
-        vote: function() {
-            this.votes++;
-        }
-    },
-    {
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
         id: 2,
-        name: "Name2",
-        slogan: "Slogan2",
-        subject: {
-            name: "sujets1",
-            description:  "description",
-        },
-        User: [
-            {
-                id: 1,
-                username: 'user2',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/images.png'
-            },
-        ],
-        University:
-        {
-            name: "university2",
-            url:'/HEI.png'
-        },
-        votes: 0,
-        vote: function() {
-            this.votes++;
-        }
-    },
-    {
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
         id: 3,
-        name: "Name2",
-        slogan: "Slogan2",
-        subject: {
-            name: "sujets1",
-            description:  "description",
-        },
-        User: [
-            {
-                id: 1,
-                username: 'user2',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/images.png'
-            },
-            {
-                id: 2,
-                username: 'user2',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/images.png'
-            },
-        ],
-        University:
-        {
-            name: "university2",
-            url:'/HEI.png'
-        },
-        votes: 0,
-        vote: function() {
-            this.votes++;
-        }
-    },
-    {
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
         id: 4,
-        name: "Name2",
-        slogan: "Slogan2",
-        subject: {
-            name: "sujets1",
-            description:  "description",
-        },
-        User: [
-            {
-                id: 1,
-                username: 'user2',
-                email: 'email',
-                password: 'password',
-                profile_picture: '/images.png'
-            },
-        ],
-        University:
-        {
-            name: "university2",
-            url: "/HEI.png",
-        },
-        votes: 0,
-        vote: function() {
-            this.votes++;
-        }
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
+        id: 5,
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
+        id: 6,
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/image.png",
+      },
+    ],
+    University: {
+      name: "niversity1",
+      url: "/HEI.png",
     },
+    votes: 0,
+    vote: function () {
+      this.votes++;
+    },
+  },
+  {
+    id: 2,
+    name: "team 2",
+    slogan: "Slogan2",
+    subject: {
+      name: "sujets1",
+      description: "description",
+    },
+    User: [
+      {
+        id: 1,
+        username: "user2",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+    ],
+    University: {
+      name: "university2",
+      url: "/HEI.png",
+    },
+    votes: 0,
+    vote: function () {
+      this.votes++;
+    },
+  },
+  {
+    id: 3,
+    name: "team 3",
+    slogan: "Slogan2",
+    subject: {
+      name: "sujets1",
+      description: "description",
+    },
+    User: [
+      {
+        id: 1,
+        username: "user2",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
+        id: 2,
+        username: "user2",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+    ],
+    University: {
+      name: "university2",
+      url: "/HEI.png",
+    },
+    votes: 0,
+    vote: function () {
+      this.votes++;
+    },
+  },
+  {
+    id: 4,
+    name: "team 4",
+    slogan: "Slogan2",
+    subject: {
+      name: "sujets1",
+      description: "description",
+    },
+    User: [
+      {
+        id: 1,
+        username: "user2",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+    ],
+    University: {
+      name: "university2",
+      url: "/HEI.png",
+    },
+    votes: 0,
+    vote: function () {
+      this.votes++;
+    },
+  },
+  {
+    id: 5,
+    name: "team 5",
+    slogan: "Slogan1",
+    subject: {
+      name: "sujet 1",
+      description: "description",
+    },
+    User: [
+      {
+        id: 1,
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
+        id: 2,
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
+        id: 3,
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
+        id: 4,
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
+        id: 5,
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/images.png",
+      },
+      {
+        id: 6,
+        username: "user1",
+        email: "email",
+        password: "password",
+        profile_picture: "/image.png",
+      },
+    ],
+    University: {
+      name: "niversity1",
+      url: "/HEI.png",
+    },
+    votes: 0,
+    vote: function () {
+      this.votes++;
+    },
+  },
 ];
